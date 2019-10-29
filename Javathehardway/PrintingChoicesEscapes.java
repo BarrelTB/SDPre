9448ec0d5a9933cc9cbd646c2a0f57ff68445b41
cwhitford <BarrelTB@gmail.com>
2019-10-28 22:19:16 -0500

    finish jthw ex 4 study drill number 2

---
+++ Javathehardway/PrintingChoicesEscapes.java
@@ -0,0 +1,5 @@
+public class PrintingChoicesEscapes {
+  public static void main( String[] args ) {
+    System.out.println( "Alpha\nBravo\nCharlie\nDelta\n\nEcho\nFoxtrot\nGolf\nHotel\n\nIndia\n\nThis is a test.\n\nI am learning Java The Hard Way!" );
+  }
+}
